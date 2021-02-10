1480576f56b2c659299621df3b00852d67b70b96
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-10 14:39:22 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,3 +5,6 @@ class my:
     BOT_TOKEN = os.environ["BOT_TOKEN"]
     API_ID = int(os.environ["API_ID"])
     API_HASH = os.environ["API_HASH"]
+    # Banned Unwanted Members..
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS","").split())
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
